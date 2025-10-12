02ff59d69a33031cbe727dca6db6d114355fcd9c
Kirill <kirillsavorin@gmail.com>
2025-10-12 09:00:54 +0200

    SB: reinstall

---
+++ dagbok-spring-api/src/test/java/cloud/dagbok/dagbok_spring_api/DagbokSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.dagbok.dagbok_spring_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DagbokSpringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
