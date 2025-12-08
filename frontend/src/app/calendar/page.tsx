5cc1af44603eacf8fdaa60b7f398f7f7d90a25aa
Kirill <127823714+Kirill9m@users.noreply.github.com>
2025-12-08 23:55:08 +0100

    Big refactor (#20)
    * Big refactoring, structure and more
    
    * prettier
    
    * CodeRabbit

---
+++ frontend/src/app/calendar/page.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Kalender | Dagbok",
+  description: "Planera din månad och skriv anteckningar per dag.",
+};
+
+import CalendarHandler from "./CalendarHandler";
+
+const CalendarPage = () => {
+  return <CalendarHandler />;
+};
+
+export default CalendarPage;
---
+++ frontend/src/app/components/Header.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import CheckAuthStatus from "@/app/(user)/auth/CheckAuthStatus";
+
+const navLinks = [
+  { name: "Calender", href: "/calendar" },
+  { name: "Profil", href: "/profile" },
+  { name: "Inställningar", href: "/settings" },
+  { name: "Admin", href: "/admin" },
+];
+
+const Header = () => {
+  const pathname = usePathname();
+
+  const user = CheckAuthStatus();
+
+  return (
+    <header className="flex items-center justify-between px-3 py-3 sm:px-6 lg:px-6 border-b-2 border-b-[#3F3D3D] bg-[#2A2A2A]">
+      <div className={"flex items-center gap-10"}>
+        <Link href="/">
+          <Image
+            src="/icon.svg"
+            alt="Dagbok Cloud Logo"
+            width={32}
+            height={32}
+          />
+        </Link>
+        <nav className="flex items-center gap-6 lg:px-6">
+          {navLinks.map((link) => {
+            const isActive =
+              pathname === link.href ||
+              (pathname.startsWith(link.href) && link.href !== "/");
+            return (
+              <Link
+                key={link.name}
+                href={link.href}
+                className={`px-3 py-1 rounded-md transition-colors duration-300 ${
+                  isActive ? "bg-[#FF7518]/60" : "hover:bg-[#FF7518]/60"
+                }`}
+              >
+                {link.name}
+              </Link>
+            );
+          })}
+        </nav>
+      </div>
+      {user === null ? <Link href="/login">Login</Link> : null}
+    </header>
+  );
+};
+
+export default Header;
